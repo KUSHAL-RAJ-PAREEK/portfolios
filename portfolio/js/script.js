e71049e4061de8381cdddf6536f257507988fd97
KUSHAL RAJ PAREEK <115624967+KUSHAL-RAJ-PAREEK@users.noreply.github.com>
2023-09-03 16:48:33 +0530

    added

---
+++ portfolio/js/script.js
@@ -0,0 +1,7 @@
+/* ==================== typing animatiom =============== */
+var typed = new Typed(".typing",{
+    strings:["","Android Developer","Graphic Designer","DSA Solver","YouTuber"],
+    typeSpeed:100,
+    backspeed:60,
+    loop:true
+})
